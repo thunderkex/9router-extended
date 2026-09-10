6aa6bbec3b7d5becafdbe2162817b0032699e362
617929zcxc <101911800+617929zcxc@users.noreply.github.com>
2026-09-10 18:53:23 +0700

    feat(providers): add standalone Qwen provider

--- open-sse/providers/registry/index.js
+++ open-sse/providers/registry/index.js
@@ -124,7 +124,7 @@ import p119 from "./selfhosted-embedding.js";
 import p120 from "./fish-audio.js";
 import p121 from "./alitp-intl.js";
 import p122 from "./xquik.js";
-
+import p124 from "./qwen.js";
 export default [
   p0,
   p1,
@@ -249,4 +249,5 @@ export default [
   p120,
   p121,
   p122,
+  p124,
 ];
